9f799493e56a80e5e1b3dc4145c8086dd7bb70ff
KarpovaYana <117508471+KarpovaYana@users.noreply.github.com>
2024-03-05 20:12:34 +0800

    Add files via upload

---
+++ Калькулятор ООП_Карпова/Калькулятор ООП_Карпова/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Калькулятор_ООП_Карпова
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool shiftDown = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textCalc.Text = textCalc.Text.ToString()+((Button)sender).Name.ToCharArray()[1];
+        }
+
+        private string checkSign(string sign)
+        {
+            switch(sign)
+            {
+                case "procent": return "%";
+                case "div": return "/";
+                case "multiply": return "*";
+                case "minus": return "-";
+                case "plus": return "+";
+                case "scopeLeft": return "(";
+                case "scopeRight": return ")";
+            }
+            return "";
+        }
+
+        private void sign_Click(object sender, RoutedEventArgs e)
+        {
+            textCalc.Text = textCalc.Text.ToString()+checkSign(((Button)sender).Name.ToString());
+        }
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+            textCalc.Text = "";
+        }
+
+        private void equals_Click(object sender, RoutedEventArgs e)
+        {
+            textCalc.Text = RPN.Calculate(textCalc.Text.ToString()).ToString();
+        }
+
+        private void textCalc_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            e.Handled = false;
+            char key='a';
+            foreach(char c in e.Key.ToString())
+                key = c;
+            if (key == 't') shiftDown = true;
+        }
+
+        private void textCalc_KeyUp(object sender, KeyEventArgs e)
+        {
+            char key = 'a';
+            e.Handled = false;
+            foreach (char c in e.Key.ToString())
+                key = c;
+            if (key == 't')
+            {
+                shiftDown = false;
+                return;
+            }
+            if(shiftDown)
+            {
+                if (key == '5') key = '%';
+                if (key == '8') key = '*';
+                if (key == '9') key = '(';
+                if (key == '0') key = ')';
+            }
+            if (!((key >= 47 && key < 57) || key == '%' || key == '*' || key == 's' || key == '-' || key == 'n' || key == '(' || key == ')'))
+            {
+                textCalc.Text = textCalc.Text.Remove(textCalc.Text.Length - 1);
+                textCalc.Select(textCalc.Text.Length,0);
+            }
+        }
+
+        private void minusx_Click(object sender, RoutedEventArgs e)
+        {
+            textCalc.Text = textCalc.Text.Remove(textCalc.Text.Length - 1);
+            textCalc.Select(textCalc.Text.Length, 0);
+        }
+    }
+}
---
+++ Калькулятор ООП_Карпова/Калькулятор ООП_Карпова/RPN.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Калькулятор_ООП_Карпова
+{
+    class RPN
+    {
+        static public double Calculate(string input)
+        {
+            string output = GetExpression(input); //Преобразовываем выражение в постфиксную запись
+            double result = Counting(output);
+            return result;
+        }
+        static private string GetExpression(string input)
+        {
+            string output = string.Empty; //Строка для хранения выражения
+            Stack<char> operStack = new Stack<char>(); //Стек для хранения операторов
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                //Разделители пропускаем
+                if (IsDelimeter(input[i]))
+                    continue;
+
+                if (Char.IsDigit(input[i]))
+                {
+                    //Читаем до разделителя или оператора, что бы получить число
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        output += input[i];
+                        i++;
+
+                        if (i == input.Length) break;
+                    }
+
+                    output += " "; //Дописываем после числа пробел в строку с выражением
+                    i--; //Возвращаемся на один символ назад, к символу перед разделителем
+                }
+
+                if (IsOperator(input[i])) //Если оператор
+                {
+                    if (input[i] == '(')
+                        operStack.Push(input[i]);
+                    else
+                    if (input[i] == ')')
+                    {
+                        char s = operStack.Pop();
+
+                        while (s != '(')
+                        {
+                            output += s.ToString() + ' ';
+                            s = operStack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        if (operStack.Count > 0)
+                            if (GetPriority(input[i]) <= GetPriority(operStack.Peek()))
+                                output += operStack.Pop().ToString() + " ";
+                        operStack.Push(char.Parse(input[i].ToString())); //Если стек пуст, или же приоритет оператора выше - добавляем операторов на вершину стека
+
+                    }
+                }
+            }
+
+            //Когда прошли по всем символам, выкидываем из стека все оставшиеся там операторы в строку
+            while (operStack.Count > 0)
+                output += operStack.Pop() + " ";
+
+            return output;
+        }
+        static private double Counting(string input)
+        {
+            double result = 0;
+            Stack<double> temp = new Stack<double>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                //Если символ - цифра, то читаем все число и записываем на вершину стека
+                if (Char.IsDigit(input[i]))
+                {
+                    string a = string.Empty;
+
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        a += input[i];
+                        i++;
+                        if (i == input.Length) break;
+                    }
+                    temp.Push(double.Parse(a));
+                    i--;
+                }
+                else if (IsOperator(input[i]))
+                {
+                    //Берем два последних значения из стека
+                    double a = temp.Pop();
+                    double b = temp.Pop();
+
+                    switch (input[i])
+                    {
+                        case '+': result = b + a; break;
+                        case '-': result = b - a; break;
+                        case '*': result = b * a; break;
+                        case '/': result = b / a; break;//Math.Pow(double.Parse(b.ToString()), double.Parse(a.ToString())).ToString()
+                        case '%': result = double.Parse(b.ToString())%double.Parse(a.ToString()); break;
+                    }
+                    temp.Push(result); //Результат вычисления записываем обратно в стек
+                }
+            }
+            return temp.Peek(); //Забираем результат всех вычислений из стека и возвращаем его
+        }
+        static private bool IsDelimeter(char c)
+        {
+            if ((" =".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+
+        static private bool IsOperator(char с)
+        {
+            if (("+-/*%()".IndexOf(с) != -1))
+                return true;
+            return false;
+        }
+
+        static private byte GetPriority(char s)
+        {
+            switch (s)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 3;
+                case '*': return 4;
+                case '/': return 4;
+                case '%': return 5;
+                default: return 6;
+            }
+        }
+    }
+}
